9b79cfac476d69c4772f24b3b993d48042310bbe
Asif Arman Saikot <49749089+bdetopup@users.noreply.github.com>
2025-09-07 20:09:35 +0600

    Update vercel-class.php

--- themes/gateway/vercel/vercel-class.php
+++ themes/gateway/vercel/vercel-class.php
@@ -1,11 +1,11 @@
 <?php
     $theme_meta = [
-        'Theme Name'        => 'PipraPay Vercel - Free',
+        'Theme Name'        => 'PipraPay Vercel Updated',
         'Theme URI'         => 'https://piprapay.com/',
         'Description'       => 'Responsive, lightweight, fast, and modern UI for PipraPay.',
-        'Version'           => '1.0.0',
-        'Author'            => 'PipraPay',
-        'Author URI'        => 'https://piprapay.com/',
+        'Version'           => '1.1.0',
+        'Author'            => 'Asif Arman Saikot',
+        'Author URI'        => 'https://bdetopup.com/',
         'License'           => 'GPL-2.0+',
         'License URI'       => 'http://www.gnu.org/licenses/gpl-2.0.txt',
         'Text Domain'       => 'piprapay-vercel',
